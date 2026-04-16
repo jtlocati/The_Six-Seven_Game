0513fa9f46ee6f0e1285272c58e69831aabd1ef7
jet locati <jetlocati4922@gmail.com>
2026-04-16 11:52:49 -0600

    Update CSS/JS loading

--- sixseven/settings.py
+++ sixseven/settings.py
@@ -33,7 +33,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "sixseven.urls"
 
